f62b68a9f9c5fb02bc13a6e1e9106f2873906179
rasguy92 <ftrouw@protonmail.com>
2023-08-18 00:55:48 +0700

    feat: add wagmi cli and export contracts to app

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,10 @@ export {dateFromTs} from './date.ts'
 export {
  parseIntFromBigNumber
 } from './ethers-helpers.ts'
+export { deployed } from './deployment.ts'
+export { 
+ wegaErc20DummyABI as dummyTokenABI, 
+ wegaErc20DummyAddress as dummyTokenAddress,
+ wegaErc20EscrowABI as escrowABI,
+ wegaErc20EscrowAddress as escrowAddress, 
+} from './abis.ts'
