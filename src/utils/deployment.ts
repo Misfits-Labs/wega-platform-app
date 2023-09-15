b2e2c224fa6346a6f5b60a7e816bbc3c88af6dd9
rasguy92 <ftrouw@protonmail.com>
2023-09-15 15:10:32 +0700

    fixup! feat: new dice design

--- src/utils/deployment.ts
+++ src/utils/deployment.ts
@@ -5,20 +5,16 @@ import _ from 'lodash';
 type Deployed = {
  [x: string]: `0x${string}` | Record<number, `0x${string}`> | undefined  
 }
-
-type SupportedContracts = ("WegaERC20Escrow" | "WegaERC20Dummy")[] 
-
 type Networks = (keyof typeof deployedAddresses.networks)[]; 
-
-
 function parseAddresses(): Deployed { 
   const deployments = {
   }
   const networks: Networks = Object.keys(JSON.parse(JSON.stringify(deployedAddresses.networks))) as any;  
   
   networks.forEach((network) => {
+    const contracts = deployedAddresses.networks[network].contracts;
+    type SupportedContracts = (keyof typeof contracts)[];
     const contractNames: SupportedContracts = Object.keys(deployedAddresses.networks[network].contracts) as any; 
-    
     contractNames.forEach((name) => {
       const obj = {
         [name]: { 
