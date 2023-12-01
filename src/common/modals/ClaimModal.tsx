eec2da93f99ae767c7ad94ff985cb0bdf288530c
rasguy92 <ftrouw@protonmail.com>
2023-12-01 14:03:32 +0700

    feat: updat ui for claiming

--- src/common/modals/ClaimModal.tsx
+++ src/common/modals/ClaimModal.tsx
@@ -5,13 +5,13 @@ import { NormalText, SmallText } from '../../components/CreateGameCard/types';
 import { ArrowDownIconV2, DownloadIcon } from '../../assets/icons';
 import { BadgeIcon, renderWagerBadge } from "../GameBar";
 import { HexishString, Wega } from '../../models';
-import { useBlockchainApiHooks } from '../../hooks';
+import { useBlockchainApiHooks, useTokenUSDValue } from '../../hooks';
 import { miniWalletAddress, capitalize } from '../../utils';
 import Button from '../Button';
 import WalletAvatar from "../../common/WalletAvatar";
 
 
-
+// TODO add gas costs
 
 export interface ClaimModalProps { 
   hide: any;
@@ -27,6 +27,8 @@ export const ClaimModal = ({ hide, game, wallet
     await claim(game.wager.wagerHash as HexishString).unwrap();
     hide(); 
   };
+  const wagerUSDValue = useTokenUSDValue(game.wager.wagerCurrency, Number(formatEther(game.wager.wagerAmount)));
+  // console.log(game.wager)
   return (
    <WinnerDeclarationContainer tw="items-start p-[24px] gap-y-[16px] min-w-[340px]">
     <div tw="flex justify-end w-full">
@@ -38,15 +40,15 @@ export const ClaimModal = ({ hide, game, wallet
     </div>
     
     <div tw="flex flex-row items-center gap-[10px] rounded-[5px] dark:bg-[#414141] py-[8px] px-[16px]">
-     <SmallText>Effective price: 1 {game.wager.wagerCurrency} = 1.01 USD </SmallText>
+     <SmallText>Effective price: 1 {game.wager.wagerCurrency} = { wagerUSDValue.loading ? 'loading..' : `${wagerUSDValue.value}`} USD </SmallText>
     </div>
 
     {/* mid section */}
-    <div tw="flex flex-col gap-y-[16px] justify-center items-center w-full">
+    <div tw="flex flex-col gap-y-[11px] justify-center items-start w-full">
      {/* top */}
-     <div tw="flex flex-col gap-y-[8px] min-w-[150px]">
+     <div tw="flex flex-col gap-y-[8px] min-w-[150px] w-full">
       <NormalText tw="text-shinishi">Amount</NormalText>
-      <div tw="flex flex-row gap-x-[11px]">
+      <div tw="flex flex-row gap-x-[11px] items-center rounded-[5px] bg-[#3A3A3A] w-full p-[5px]">
        <BadgeIcon size="44px">{renderWagerBadge(game.wager.wagerType, game.wager.wagerCurrency)}</BadgeIcon>
        <div tw="flex flex-col gap-y-[11px]">
         <NormalText tw="text-blanc">{formatEther(game.wager.wagerAmount)} USDC</NormalText>
@@ -54,15 +56,17 @@ export const ClaimModal = ({ hide, game, wallet
        </div>
       </div>
      </div>
-     
-     <div tw="rounded-[100%] dark:bg-[#414141] p-[5px] flex justify-center">
-      <ArrowDownIconV2 />
+
+     <div tw="w-full flex justify-center">
+      <div tw="rounded-[100%] dark:bg-[#414141] flex justify-center">
+        <ArrowDownIconV2 />
+      </div>
      </div>
 
      {/* bottom */}
-     <div tw="flex flex-col gap-y-[8px] min-w-[150px]">
+     <div tw="flex flex-col gap-y-[8px] min-w-[150px] w-full">
       <NormalText tw="text-shinishi">Destination</NormalText>
-      <div tw="flex flex-row gap-x-[11px]">
+      <div tw="flex flex-row gap-x-[11px] items-center rounded-[5px] bg-[#3A3A3A] w-full p-[5px]">
        <ClaimModalAvatarWrapper>
         <WalletAvatar 
           address={wallet.address} 
@@ -80,7 +84,6 @@ export const ClaimModal = ({ hide, game, wallet
 
     {/* details */}
     <div tw="flex flex-col items-start gap-y-[16px] items-center w-full">
-     
      <div tw="flex flex-row items-end justify-between w-full">
       <NormalText tw="font-bold">Claim details</NormalText>
       <div tw="flex flex-row items-center">
@@ -89,24 +92,36 @@ export const ClaimModal = ({ hide, game, wallet
        <NormalText tw="dark:text-shinishi">USD</NormalText>
       </div>
      </div>
-
      <div tw="flex flex-row items-end justify-between w-full">
       <NormalText tw="dark:text-shinishi">Total claim before fees</NormalText>
       <NormalText>{formatEther(game.wager.wagerAmount)} {game.wager.wagerCurrency}</NormalText>
      </div>
-     <div tw="flex flex-row items-end justify-between w-full">
+
+     {/* <div tw="flex flex-row items-end justify-between w-full">
       <NormalText tw="dark:text-shinishi">Gas costs</NormalText>
       <NormalText>0.0020 MATIC</NormalText>
-     </div>
-     <div tw="flex flex-row items-end justify-between w-full">
-      <NormalText tw="dark:text-shinishi">Claim fees (1%)</NormalText>
-      <NormalText>00.00 {game.wager.wagerCurrency}</NormalText>
-     </div>
+    </div> */}
     </div>
-    <div tw="flex flex-row items-center gap-[10px] rounded-[5px] dark:bg-[#414141] py-[8px] px-[16px]">
-     <NormalText tw="dark:text-shinishi">What you should receive after fees</NormalText>
-     <NormalText>{formatEther(game.wager.wagerAmount)} {game.wager.wagerCurrency}</NormalText>
+    <div tw="w-full flex flex-col items-center justify-center gap-y-[8px]">
+      <NormalText tw="dark:text-shinishi text-center">Fee details</NormalText>
+      <div tw="w-full rounded-[4px] border border-[1px] border-shinishi p-[12px]">
+        <div tw="flex flex-row items-end justify-between w-full">
+          <NormalText tw="dark:text-shinishi">Platform fee (5%)</NormalText>
+          <NormalText>00.00 {game.wager.wagerCurrency}</NormalText>
+        </div>
+      </div>
+    </div>  
+    
+    <div tw="self-start flex flex-row items-center gap-[10px] py-[8px] px-[5px]">
+      <NormalText tw="dark:text-shinishi">You pay</NormalText>
+      <NormalText>{formatEther(game.wager.wagerAmount)} {game.wager.wagerCurrency}</NormalText>
     </div>
+
+    <div tw="self-start flex flex-row items-center justify-between gap-[10px] rounded-[5px] dark:bg-[#414141] py-[8px] px-[5px] w-full">
+      <NormalText tw="dark:text-shinishi">You receive</NormalText>
+      <NormalText>{formatEther(game.wager.wagerAmount)} {game.wager.wagerCurrency}</NormalText>
+    </div>
+
     <Button buttonType="primary" className="flex items-center justify-center w-full" onClick={() => handleClaimClick()} >
      { isClaimingLoading ? "Loading..." : "Claim" }
      <DownloadIcon tw="h-[16px] w-[16px] ms-[5px] dark:stroke-blanc" />
