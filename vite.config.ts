aad88561e6bab4e8c26410f5d3c0bc0e45b35e25
rasguy92 <ftrouw@protonmail.com>
2023-12-02 10:48:37 +0700

    fixup! fixup! fixup! fixup! fixup! feat: builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  console.log(mode)
   process.env = { ...process.env, ...loadEnv(mode, process.cwd())};
   return defineConfig({
     // server: { https: true },
